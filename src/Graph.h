97995b799d86eb26ec01a62f2270024798fae03b
Yi Yao <leaf_yao8621@hotmail.com>
2017-10-14 20:22:04 -0400

    modifications

--- src/Graph.h
+++ src/Graph.h
@@ -1,6 +1,7 @@
 typedef struct Graph Graph;
 
-Graph* new_Graph(int size)；
+Graph* new_Graph(int size);
+int init(int num);
 int add_Vertex(Graph* g);
 int add_conn(Graph* g, int from, int to);
 int free_Graph(Graph* g)
