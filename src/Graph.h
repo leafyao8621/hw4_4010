04b1d53fdaaa311325621795716b5c5bc9413ddc
Yi Yao <leaf_yao8621@hotmail.com>
2017-10-25 15:26:35 -0400

    modifications

--- src/Graph.h
+++ src/Graph.h
@@ -1,11 +1,11 @@
 typedef struct Graph Graph;
 
-Graph* new_Graph(void);
-int init(Graph* g, int num);
-int add_Vertex(Graph* g);
-int remove_Vertex(Graph* g, int ind);
-int pick_vertex(Graph* g, int* opt);
-int add_conn(Graph* g, int from, int to);
-int free_Graph(Graph* g);
-int print_Graph(Graph* g);
-int check_conn(Graph* g, int* opt);
+Graph* new_Graph(void);//creates a new graph
+int init(Graph* g, int num);//initializes a new graph
+int add_Vertex(Graph* g);//adds a new vertex
+int remove_Vertex(Graph* g, int ind);//removes a vertex
+int pick_vertex(Graph* g, int* opt);//picks a vertex to be removed
+int add_conn(Graph* g, int from, int to);//adds a connection(directed edge)
+int free_Graph(Graph* g);//frees memory occupied
+int print_Graph(Graph* g);//prints out a represention
+int check_conn(Graph* g, int* opt);//checks whether connected
